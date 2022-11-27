93495cd34ba7f0e736e29e5463ae5ad30d229bd4
mohammad <moerfani78@gmail.com>
2022-11-27 17:39:32 +0330

    fix: fix view of show order for restaurant= add total price to it

--- resources/views/restaurant/food/editFood.blade.php
+++ resources/views/restaurant/food/editFood.blade.php
@@ -107,7 +107,7 @@
                     <select name="discount" id="discount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="{{null}}" selected>Without Discount</option>
                     @foreach($discounts as $discount)
-                            <option value="{{$discount->id}}">{{$discount->title}}=>({{$discount->percentage}})</option>
+                            <option value="{{$discount->id}}" {{$discount->id== $food->discount_id?'selected':''}}>{{$discount->title}}=>({{$discount->percentage}})</option>
                         @endforeach
                     </select>
                 </div>
