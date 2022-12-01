6bdc149d1cd50d91a7137af67523f694b22c46bd
mohammad <moerfani78@gmail.com>
2022-12-02 03:02:01 +0330

    fix: fix bug of setCurrent in AddressController

--- app/Http/Controllers/user/AddressController.php
+++ app/Http/Controllers/user/AddressController.php
@@ -18,31 +18,31 @@ class AddressController extends Controller
     public function index()
     {
         $addresses = auth()->user()->addresses;
-        return $addresses->isNotEmpty() ? $addresses:["msg"=>"you don't add any address"];
+        return $addresses->isNotEmpty() ? $addresses : ["msg" => "you don't add any address"];
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  AddressRequest  $request
+     * @param AddressRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(AddressRequest $request)
     {
         $address = auth()->user()->addresses()->save(new Address($request->all()));
         $this->setCurrent($address);
-        return response(['msg'=>'Address added Successfully']);
+        return response(['msg' => 'Address added Successfully']);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Address  $address
+     * @param \App\Models\Address $address
      * @return \Illuminate\Http\Response
      */
     public function show(Address $address)
     {
-        return response($address,200);
+        return response($address, 200);
 
     }
 
@@ -50,32 +50,35 @@ class AddressController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  AddressRequest  $request
-     * @param  \App\Models\Address  $address
+     * @param AddressRequest $request
+     * @param \App\Models\Address $address
      * @return \Illuminate\Http\Response
      */
     public function update(AddressRequest $request, Address $address)
     {
         $address->update($request->all());
-        return response(['msg'=>'Address updated Successfully']);
+        return response(['msg' => 'Address updated Successfully']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Address  $address
+     * @param \App\Models\Address $address
      * @return \Illuminate\Http\Response
      */
     public function destroy(Address $address)
     {
         $address->delete();
-        return response(['msg'=>'Address Deleted']);
+        return response(['msg' => 'Address Deleted']);
     }
 
     public function setCurrent(Address $address)
     {
-        auth()->user()->addresses()->where('is_current',1)->update(['is_current'=>null]);
-        $address->update(['is_current'=>true]);
-        return response(["your current address set ".$address->title]);
+        if ($address->addressable_id !== auth()->user()->id) {
+            return response(['msg' => "Please Enter Correct Address"]);
+        }
+        auth()->user()->addresses()->where('is_current', 1)->update(['is_current' => null]);
+        $address->update(['is_current' => true]);
+        return response(["your current address set " . $address->title]);
     }
 }
